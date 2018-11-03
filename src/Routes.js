748c3829f8cb2f71ac6a7b4fbc98ce80ee1c3795
potchimi <karunakara.potchimireddy@hpe.com>
2018-11-03 19:00:37 +0530

    Changing routes file

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
     Route, Switch, Redirect, withRouter,
 } from 'react-router-dom';
+import { compact } from 'underscore';
 import Home from './components/Home';
 import About from './components/About';
 
-class Routes extends Component {
-    render() {
-        return (
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path="/about" component={About}/>
-                <Redirect to='/'/>
-            </Switch>
-        );
-    }
+function buildUrl(...args) {
+    return `/${compact(args.map(item => (item ? item.toString().replace(/^\/|\/$/g, '') : undefined))).join('/')}`;
 }
+
+const Routes = ({ match }) => (
+    <Switch>
+        <Route exact path={buildUrl(match.url, '/')} component={Home}/>
+        <Route path={buildUrl(match.url, '/about')} component={About}/>
+        <Redirect to='/'/>
+    </Switch>
+);
+
+Routes.propTypes = {
+    match: PropTypes.shape({
+        path: PropTypes.string.isRequired,
+    }),
+};
+
 export default withRouter(Routes);
